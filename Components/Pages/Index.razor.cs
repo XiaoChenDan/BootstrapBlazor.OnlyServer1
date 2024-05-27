a1adeed38261e7e0d3c48e189d83fb9d96df43e8
XiaoChenDan <justeought@outlook.com>
2024-05-27 15:53:00 +0800

    添加案例：table动态呈现时，高度不正确

--- Components/Pages/Index.razor.cs
+++ Components/Pages/Index.razor.cs
@@ -5,48 +5,24 @@ namespace BootstrapBlazor.OnlyServer1.Components.Pages
 {
     public partial class Index
     {
-        private static List<TreeViewItem<TreeFoo>> GetLazyItems()
+        private List<Foo>? Items { get; set; }
+
+        private bool ShowDialog { get; set; } = false;
+
+        private string DialogStyleString => ShowDialog ? "display:flex;" : "display:none;";
+
+        protected override void OnInitialized()
         {
-            var ret =new List<TreeViewItem<TreeFoo>>
-            {
-                new TreeViewItem<TreeFoo>(new TreeFoo())
-                {
-                    Text="懒加载父节点1",
-                    HasChildren=true
-                },
-                new TreeViewItem<TreeFoo>(new TreeFoo())
-                {
-                    Text="懒加载父节点2",
-                    HasChildren=false
-                }
-            };
-            return ret;
+            base.OnInitialized();
+            //获取随机数据
+            //Get random data
+            Items = Foo.GenerateFoo(FooLocalizer);
         }
 
-        private static async Task<IEnumerable<TreeViewItem<TreeFoo>>> OnExpandNodeAsync(TreeViewItem<TreeFoo> node)
+        private void OpenDialog()
         {
-            await Task.Delay(800);
-            var item = node.Value;
-            return new TreeViewItem<TreeFoo>[]
-            {
-                new(new TreeFoo()
-                {
-                    Id = $"{item.Id}-101",
-                    ParentId = item.Id
-                })
-                {
-                    Text = "懒加载子节点1",
-                    HasChildren = true
-                },
-                new(new TreeFoo()
-                {
-                    Id = $"{item.Id}-102",
-                    ParentId = item.Id
-                })
-                {
-                    Text = "懒加载子节点2",
-                    CheckedState = CheckboxState.Checked
-                } };
+            ShowDialog = !ShowDialog;
+            StateHasChanged();
         }
     }
 }
